25c932253f3708eec1e741a9a5afd563a065f98d
joeveloper <bebosir@gmail.com>
2023-03-07 02:05:06 +0100

    fixed linter errors

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 function openNav() {
-    document.getElementById("mobile-menu").style.visibility = "visible";
-  }
+  document.getElementById('mobile-menu').style.visibility = 'visible';
+}
 
-  function closeNav() {
-    document.getElementById("mobile-menu").style.visibility = "hidden";
-  }
+function closeNav() {
+  document.getElementById('mobile-menu').style.visibility = 'hidden';
+}
+
+openNav();
+
+closeNav();
